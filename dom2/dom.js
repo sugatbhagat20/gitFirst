074b3d0b663298cb80798d8f0e2e2074b0fc11de
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-07-28 01:57:01 +0530

    dom(Task 4)

--- dom2/dom.js
+++ dom2/dom.js
@@ -1,7 +1,15 @@
 // headerTitle.textContent = "SUG";
 
-headerTitle.style.border = "3px solid black";
-var titl = document.getElementsByClassName("title");
+// headerTitle.style.border = "3px solid black";
+// var titl = document.getElementsByClassName("title");
 
-titl.style.color = "green";
-titl.style.fontWeight = "bold";
+// titl.style.color = "green";
+// titl.style.fontWeight = "bold";
+
+var items = document.getElementsByClassName("list-group-item");
+
+console.log(items);
+items[2].style.background = "#00ff00"; //change item 3 bg to green
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
