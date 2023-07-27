7e14fde3ec4c7d8fcf820c045aeb2e6390bead9a
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-07-28 02:47:06 +0530

    dom(task5)

--- dom2/dom.js
+++ dom2/dom.js
@@ -6,10 +6,15 @@
 // titl.style.color = "green";
 // titl.style.fontWeight = "bold";
 
-var items = document.getElementsByClassName("list-group-item");
+var li = document.getElementsByTagName("li");
+// console.log(last);
 
-console.log(items);
-items[2].style.background = "#00ff00"; //change item 3 bg to green
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
-}
+// var li = document.getElementsByTagName("li");
+
+// console.log(li);
+// li[2].style.background = "#00ff00"; //change item 3 bg to green
+
+li[4].style.fontWeight = "bold";
+li[4].style.fontFamily = "Helvetica Neue";
+
+// li[4].style.backgroundColor = "#yellow";
