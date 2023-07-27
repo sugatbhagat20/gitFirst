b5152808b9f43db61a527030b2210c49f0d90221
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-07-27 23:39:11 +0530

    second commit

---
+++ dom2/dom.js
@@ -0,0 +1,7 @@
+// headerTitle.textContent = "SUG";
+
+headerTitle.style.border = "3px solid black";
+var titl = document.getElementsByClassName("title");
+
+titl.style.color = "green";
+titl.style.fontWeight = "bold";
