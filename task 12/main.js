247eddadd06b57287d1bcc5ca006a052869847e6
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-07-29 16:26:59 +0530

    task13(DOM)

--- task 12/main.js
+++ task 12/main.js
@@ -4,10 +4,13 @@ var emailItem = document.querySelector("#email");
 var phoneItem = document.querySelector("#phone");
 var submitbtn = document.querySelector("#submit");
 var itemList = document.getElementById("items");
+var deletebtn = document.querySelector("#delete");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 });
+
+itemList.addEventListener("click", del);
 
 submitbtn.addEventListener("click", function () {
   let obj = {
@@ -21,10 +24,22 @@ submitbtn.addEventListener("click", function () {
   var newItem = document.getElementById("name").value;
   var newItem1 = document.getElementById("email").value;
   var newItem2 = document.getElementById("phone").value;
-  var li = document.createElement("li");
 
+  var li = document.createElement("li");
   li.appendChild(document.createTextNode(newItem));
   li.appendChild(document.createTextNode(newItem1));
-  li.appendChild(document.createTextNode(newItem2));
+
+  var deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete";
+  deleteBtn.appendChild(document.createTextNode("Delete"));
+  li.appendChild(deleteBtn);
+
   itemList.appendChild(li);
 });
+
+function del(e) {
+  if (e.target.classList.contains("delete")) {
+    var li = e.target.parentElement; //clicking on the element we want parent li
+    itemList.removeChild(li);
+  }
+}
